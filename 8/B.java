7c3565678ae3c9fb9a28b2185355ae429f75573e
qbahers <quentin.bahers@gmail.com>
2017-12-08 22:27:18 +0100

    Solve Day 8: I Heard You Like Registers (Part Two)

---
+++ 8/B.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class B {
+
+    private static boolean evalCondition(int left, String operator, int right) throws UnsupportedOperationException {
+        switch (operator) {
+            case "<": return left < right;
+            case ">": return left > right;
+            case "<=": return left <= right;
+            case ">=": return left >= right;
+            case "==": return left == right;
+            case "!=": return left != right;
+            default: throw new UnsupportedOperationException();
+        }
+    }
+
+    private static int evalOperation(int left, String operator, int right) throws UnsupportedOperationException {
+        switch (operator) {
+            case "inc": return left + right;
+            case "dec": return left - right;
+            default: throw new UnsupportedOperationException();
+        }
+    }
+
+    public static void main(String[] args) throws UnsupportedOperationException {
+        Scanner in = new Scanner(System.in);
+
+        Map<String, Integer> reg = new HashMap<String, Integer>();
+
+        int max = 0;
+        while (in.hasNextLine()) {
+            String[] ins = in.nextLine().split(" ");
+
+            if (evalCondition(reg.getOrDefault(ins[4], 0), ins[5], Integer.parseInt(ins[6]))) {
+                reg.put(ins[0], evalOperation(reg.getOrDefault(ins[0], 0), ins[1], Integer.parseInt(ins[2])));
+                max = Math.max(max, reg.get(ins[0]));
+            }
+        }
+
+        System.out.println(max);
+    }
+
+}
